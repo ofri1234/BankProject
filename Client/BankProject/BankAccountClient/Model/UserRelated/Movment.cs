5c5d2ff87037c730ad0801bbc45f4ce5f14efc8b
osimantov <osimantov@OSIMANTOV-G1>
2020-01-20 16:01:34 +0200

    User model and movment model created

---
+++ Client/BankProject/BankAccountClient/Model/UserRelated/Movment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountClient.Model.UserRelated
+{
+    enum MovmentType
+    {
+        Income,Outcome,
+    }
+    class Movment
+    {
+        public string ID { get; set; }
+        public MovmentType MType { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public Movment(MovmentType mType, int amount, DateTime date, string description = "")
+        {
+            MType = mType;
+            Amount = amount;
+            Date = date;
+            Description = description;
+        }
+
+    }
+}
